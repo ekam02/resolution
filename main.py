418e725dfaa2447ee1cdd0c392c3414a3cfdf771
Enrique Martínez Agudelo <enrique.martinezagudelo@externos-co.cencosud.com>
2025-07-03 18:52:46 -0500

    feat: agrega funcionalidad `main`
    Agrega la funcionalidad `main` en el módulo "main.py".
    Agrega el uso de logging en el módulo "main.py".

---
+++ main.py
@@ -0,0 +1,33 @@
+from config import log_console_level, log_file_level, output_dir
+from config.logger_config import setup_logger
+from models import INSERT_BILLER_RESOLUTION
+from utils.finder import get_max_resolution_id
+from utils.uploader import upload_resolutions
+
+
+logger = setup_logger(__name__, console_level=log_console_level, file_level=log_file_level)
+
+
+def main():
+    try:
+        resolutions = upload_resolutions()
+        logger.info(f"Se cargaron {len(resolutions)} resoluciones.")
+        max_id = get_max_resolution_id() + 1
+        logger.info(f"Se ha recuperador el ultimo id de la tabla `factura.resoluciones`: {max_id -1}.")
+        for resolution in resolutions:
+            resolution.id = max_id
+            max_id += 1
+        logger.info("Se han calculado y asignado los identificadores a las nuevas resoluciones.")
+        resolutions = [resolution.values for resolution in resolutions]
+        logger.info("Se han calculado los valores para la inserción de las nuevas resoluciones.")
+        output_text = INSERT_BILLER_RESOLUTION.format(",\n".join(resolutions))
+        logger.info("Se han agregado los valores sobre la inserción de las nuevas resoluciones.")
+        with open(output_dir / "resolution.sql", "+w") as file:
+            file.write(output_text)
+        logger.info("Se han generado un fichero SQL con la transacción para la inserción de las nuevas resoluciones.")
+    except Exception as e:
+        logger.exception(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
